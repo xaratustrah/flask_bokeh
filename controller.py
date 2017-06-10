1e3a80ab4d658249a827a73e009c3be0181449e9
Xaratustrah <shahab.sanjari@gmail.com>
2017-06-10 12:08:07 +0200

    added AJAX

--- controller.py
+++ controller.py
@@ -4,17 +4,40 @@
 """
 Flask plot test
 
-based on:
+inspired by:
 http://hplgit.github.io/web4sciapps/doc/web/index.html
+
+
 """
 from model import InputForm
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from flask_bootstrap import Bootstrap
 from compute import compute
 from bokeh.resources import INLINE
+import subprocess
 
 app = Flask(__name__)
 Bootstrap(app)
+
+
+def run_cmd(cmd_string):
+    try:
+        p = subprocess.Popen(cmd_string.split(),
+                             stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        ok = p.wait()
+        out, err = p.communicate()
+    except FileNotFoundError:
+        out = b''
+        err = b''
+        ok = True
+
+    return ok, out, err
+
+
+@app.route('/_get_date', methods=['GET', 'POST'])
+def _get_date():
+    ok, out, err = run_cmd('date')
+    return jsonify(result=out.decode('utf-8'))
 
 
 @app.route('/', methods=['GET', 'POST'])
